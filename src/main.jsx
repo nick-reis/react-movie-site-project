fb018e0f5fed9ef5c68c3969b76e0895bc2bac4b
nick-reis <nickreiser05@gmail.com>
2023-05-12 23:02:51 -0400

    help

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import ScrollToTop from "./components/scrollToTop.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter basename="/react-movie-site-project/#">
+    <BrowserRouter basename="/react-movie-site-project">
       <ScrollToTop />
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
